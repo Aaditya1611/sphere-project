348e239faf04f5c445da941580b4c6d332bccb42
aadityaraj1611 <aadityaraj1611@gmail.com>
2025-03-24 05:34:58 +0530

    add new page for reseting password

---
+++ sphere-frontend/src/login/forgotpasswd.jsx
@@ -0,0 +1,12 @@
+import BGimg from "../assets/loginpageBG.jpeg";
+
+const ForgotPassword = () => {
+
+    return (
+        <div className="h-screen relative">
+            <img src={BGimg} className="absolute h-full w-full left-0 top-0 object-cover -z-50"></img>
+        </div>
+    )
+}
+
+export default ForgotPassword;
