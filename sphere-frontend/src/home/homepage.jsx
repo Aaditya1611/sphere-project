046d30ecb24fbbe5c4c14c30f51d36b412c7b4c2
aadityaraj1611 <aadityaraj1611@gmail.com>
2025-03-26 06:50:31 +0530

    new landing page for authorized users

---
+++ sphere-frontend/src/home/homepage.jsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+
+    return (
+        <div>
+            Welcome to Sphere chat application, you are successfully logged in.
+        </div>
+    )
+}
+
+export default HomePage;
