b924cc5d6170da7f99410299c284505515346457
aadityaraj1611 <aadityaraj1611@gmail.com>
2025-03-26 06:40:24 +0530

    minor change

--- sphere-backend/src/main/java/com/chatapp/web/login/UserRepository.java
+++ sphere-backend/src/main/java/com/chatapp/web/login/UserRepository.java
@@ -1,9 +1,9 @@
-// package com.chatapp.web.login;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
+package com.chatapp.web.login;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-// @Repository
-// public interface UserRepository extends JpaRepository<User, Long> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 	
-// 	User findByUsername(String username);
-// }
+	User findByUsername(String username);
+}
