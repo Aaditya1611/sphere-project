7f377e4ec5dca5ad5e3babe83547ce70548f9434
aadityaraj1611 <aadityaraj1611@gmail.com>
2025-03-26 06:48:04 +0530

    new JwtResponse class for managing tokens generated by JwtUtils class

---
+++ sphere-backend/src/main/java/com/chatapp/web/login/JwtResponse.java
@@ -0,0 +1,37 @@
+package com.chatapp.web.login;
+
+public class JwtResponse {
+    
+    private String token;
+    private String type = "Bearer";
+
+    public JwtResponse() {
+
+        
+    }
+
+    public JwtResponse(String accessToken) {
+
+        this.token = accessToken;
+    }
+
+    public String getAccessToken() {
+        
+        return token;
+    }
+
+    public void setAccessToken(String accessToken){
+
+        this.token = accessToken;
+    }
+
+    public String getTokenType() {
+
+        return type;
+    }
+
+    public void setTokenType(String tokenType) {
+
+        this.type = tokenType;
+    }
+}
